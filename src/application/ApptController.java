759f03b208cae1af1fb6105edcdda05b1ebc8d80
HHills <hamunaptrahills@gmail.com>
2025-08-22 21:17:25 -0400

    FrontPage.fxml = non-dynamic elements done

--- src/application/ApptController.java
+++ src/application/ApptController.java
@@ -39,6 +39,6 @@ public class ApptController
 		  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		  scene = new Scene(root);
 		  stage.setScene(scene);
-		  //stage.show();
+		  stage.show();
 	}
 }
